05ae3a95f5a47ef061a7c530209339759e501a04
Hevsy <roomyn@gamil.com>
2023-01-10 13:26:45 +1100

    Updates

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+# from cs50 import SQL
+from sqlalchemy import create_engine, text
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
@@ -23,7 +23,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# db = SQL("sqlite:///finance.db")
+# Configure SQLALchemy instead
+engine = create_engine("sqlite:///finance.db")
+
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
@@ -43,23 +46,23 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    assets = db.execute(
-        "SELECT symbol, amount FROM assets WHERE user_id=?", session["user_id"])
-
-    # initalise the counter and portfolio
-    portfolio = []
-    grand_total = 0
-    for row in assets:
-        lookup_data = lookup(row["symbol"])
-        amount = int(row['amount'])
-        symbol_total = float(lookup_data['price']) * amount
-        grand_total += symbol_total
-        portfolio.append({"symbol": lookup_data['symbol'], "name": lookup_data['name'], "price": usd(
-            lookup_data['price']), "amount": amount, 'total': usd(symbol_total)})
-    cash = float(db.execute("SELECT cash FROM users WHERE id = ?",
-                            session["user_id"])[0]["cash"])
-    grand_total += cash
-    return render_template("index.html", portfolio=portfolio, cash=usd(cash), grand_total=usd(grand_total))
+    with engine.begin() as db:
+        assets = db.execute(text(
+            "SELECT symbol, amount FROM assets WHERE user_id= :id"), {"id": session["user_id"]}).all()
+        # initalise the counter and portfolio
+        portfolio = []
+        grand_total = 0
+        for row in assets:
+            lookup_data = lookup(row["symbol"])
+            amount = int(row["amount"])
+            symbol_total = float(lookup_data["price"]) * amount
+            grand_total += symbol_total
+            portfolio.append({"symbol": lookup_data['symbol'], "name": lookup_data['name'], "price": usd(
+                lookup_data['price']), "amount": amount, 'total': usd(symbol_total)})
+        cash = float(db.execute(text("SELECT cash FROM users WHERE id = :id"),
+                                {"id": session["user_id"]}).all()[0]["cash"])
+        grand_total += cash
+        return render_template("index.html", portfolio=portfolio, cash=usd(cash), grand_total=usd(grand_total))
 
 
 @ app.route("/buy", methods=["GET", "POST"])
@@ -80,43 +83,45 @@ def buy():
             return apology("Must provide a valid stock symbol")
 
         # Check how much cash on the users account and whether it's enough
-        cash = float(db.execute(
-            "SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"])
-        ppu = float(quote["price"])
-        # Debugging: print (cash, shares, shares*float(quote["price"]))
-        if shares*ppu > cash:
-            return apology("Insufficient funds for this operation")
+        with engine.begin() as db:
+            cash = float(db.execute(text(
+                "SELECT cash FROM users WHERE id = :id"), {"id": session["user_id"]}).all()[0]["cash"])
+            ppu = float(quote["price"])
+            # Debugging: print (cash, shares, shares*float(quote["price"]))
+            if shares*ppu > cash:
+                return apology("Insufficient funds for this operation")
 
-        # add transaction
-        db.execute("INSERT INTO transactions (user_id, datetime, symbol, amount, ppu) VALUES (?, datetime('now'), ?, ?, ?)",
-                   session["user_id"], symbol, shares, ppu)
+            # add transaction
+            db.execute(text("INSERT INTO transactions (user_id, datetime, symbol, amount, ppu) VALUES (:id, datetime('now'), :sy, :am, :ppu)"),
+                       {"id": session["user_id"], "sy": symbol, "am": shares, "ppu": ppu})
 
-        # add asset
-        # !!!! this will create duplicate rows for same assets - need to check for existing asset and update if required!!!
-        a = db.execute(
-            "SELECT amount FROM assets WHERE user_id = ? AND symbol=?", session["user_id"], symbol)
-        if not a:
-            db.execute("INSERT INTO assets (user_id, symbol, amount) VALUES (?, ?, ?)",
-                       session["user_id"], symbol, shares)
-        else:
-            a = int(a[0]['amount']) + shares
-            db.execute("UPDATE assets SET amount = ? WHERE symbol = ? AND user_id = ?",
-                       a, symbol, session["user_id"])
-        # subtract cash
-        db.execute("UPDATE users SET cash = ? WHERE id = ?",
-                   cash-shares*ppu, session["user_id"])
-        # redirect to main page when everything was done
-        return redirect("/")
+            # add asset
+            # !!!! this will create duplicate rows for same assets - need to check for existing asset and update if required!!!
+            a = db.execute(text(
+                "SELECT amount FROM assets WHERE user_id = :id AND symbol=:sy"), {"id": session["user_id"], "sy": symbol}).all()
+            if not a:
+                db.execute(text("INSERT INTO assets (user_id, symbol, amount) VALUES (:id, :sy, :am)"),
+                           {"id": session["user_id"], "sy": symbol, "am": shares})
+            else:
+                a = int(a[0]['amount']) + shares
+                db.execute(text("UPDATE assets SET amount = :am WHERE symbol = :sy AND user_id = :id"),
+                           {"id": session["user_id"], "sy": symbol, "am": shares})
+            # subtract cash
+            db.execute(text("UPDATE users SET cash = :c WHERE id = :id"),
+                       {"c": cash-shares*ppu, "id": session["user_id"]})
+            # redirect to main page when everything was done
+            return redirect("/")
 
 
 @ app.route("/history")
 @ login_required
 def history():
     """Show history of transactions"""
-    history = db.execute(
-        "SELECT symbol, amount, ppu, datetime FROM transactions WHERE user_id = ?", session["user_id"])
-    # print(history)
-    return render_template("history.html", history=history)
+    with engine.begin() as db:
+        history = db.execute(text(
+            "SELECT symbol, amount, ppu, datetime FROM transactions WHERE user_id = :id"), {"id": session["user_id"]}).all()
+        # print(history)
+        return render_template("history.html", history=history)
 
 
 @ app.route("/login", methods=["GET", "POST"])
@@ -138,18 +143,18 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?",
-                          request.form.get("username"))
+        with engine.begin() as db:
+            rows = db.execute(text("SELECT hash FROM users WHERE username = :u"),
+                              {"u": request.form.get("username")}).all()
+            # Ensure username exists and password is correct
+            if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+                return apology("invalid username and/or password", 403)
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["id"]
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
+            # Redirect user to home page
+            return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -197,14 +202,16 @@ def register():
             return apology("Passwords do not match!")
 
         # Check if username already exists
-        rows = db.execute("SELECT * FROM users WHERE username = ?",
-                          request.form.get("username"))
-        if len(rows) == 1:
-            return apology("Username already exists")
-        hash = generate_password_hash(password)
-        db.execute(
-            "INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
-        return redirect("/login")
+        with engine.begin() as db:
+            try:
+               db.execute(text("SELECT id FROM users WHERE username = :u"),
+                                {"u": request.form.get("username")}).one()
+            except:
+                return apology("Username already exists")
+            hash = generate_password_hash(password)
+            db.execute(text(
+                "INSERT INTO users (username, hash) VALUES (:u, :h)"), {"u": username, "h": hash})
+            return redirect("/login")
 
 
 @ app.route("/sell", methods=["GET", "POST"])
@@ -212,9 +219,10 @@ def register():
 def sell():
     """Sell shares of stock"""
     if request.method == "GET":
-        assets = db.execute(
-            "SELECT symbol FROM assets WHERE user_id=?", session["user_id"])
-        return render_template("/sell.html", symbols=[foo['symbol'] for foo in assets])
+        with engine.begin() as db:
+            assets = db.execute(text(
+                "SELECT symbol FROM assets WHERE user_id=?"), {"id": session["user_id"]}).all()
+            return render_template("/sell.html", symbols=[row["symbol"] for row in assets])
     if request.method == "POST":
         symbol = request.form.get("symbol")
         if not request.form.get("shares").isdigit():
@@ -224,36 +232,37 @@ def sell():
             return apology("Amount of shares must be a positive number!")
 
         # Check how much shares user have
-        a = db.execute(
-            "SELECT amount FROM assets WHERE user_id = ? AND symbol=?", session["user_id"], symbol)
-        if not a:
-            return apology("You don't own these shares")
+        with engine.begin() as db:
+            a = db.execute(text(
+                "SELECT amount FROM assets WHERE user_id = :id AND symbol=:sy"), {"id": session["user_id"], "sy": symbol}).all()
+            if not a:
+                return apology("You don't own these shares")
 
-        # Check if user owns enough shares
-        a = int(a[0]['amount'])
-        if (a < shares):
-            return apology("Not enough shares for this operation")
+            # Check if user owns enough shares
+            a = int(a[0]['amount'])
+            if (a < shares):
+                return apology("Not enough shares for this operation")
 
-        # Check how much cash on the users account and the price of the shares
-        quote = lookup(symbol)
-        if not symbol or not quote:
-            return apology("Invalid stock symbol")
-        cash = float(db.execute(
-            "SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"])
-        ppu = float(quote["price"])
+            # Check how much cash on the users account and the price of the shares
+            quote = lookup(symbol)
+            if not symbol or not quote:
+                return apology("Invalid stock symbol")
+            cash = float(db.execute(text(
+                "SELECT cash FROM users WHERE id = :id"), {"id": session["user_id"]}).all()[0]["cash"])
+            ppu = float(quote["price"])
 
-        # add transaction
-        db.execute("INSERT INTO transactions (user_id, datetime, symbol, amount, ppu) VALUES (?, datetime('now'), ?, ?, ?)",
-                   session["user_id"], symbol, -shares, ppu)
+            # add transaction
+            db.execute(text("INSERT INTO transactions (user_id, datetime, symbol, amount, ppu) VALUES (:id, datetime('now'), :sy, :am, :ppu)"),
+                       {"id": session["user_id"], "sy": symbol, "am": shares, "ppu": ppu})
 
-        #  update asset list
-        db.execute("UPDATE assets SET amount = ? WHERE symbol = ? AND user_id = ?",
-                   a - shares, symbol, session["user_id"])
-        # add cash
-        db.execute("UPDATE users SET cash = ? WHERE id = ?",
-                   cash+shares*ppu, session["user_id"])
-        # redirect to main page when everything was done
-        return redirect("/")
+            #  update asset list
+            db.execute(text("UPDATE assets SET amount = :am WHERE symbol = :sy AND user_id = :id"),
+                       {"am": a - shares, "sy": symbol, "id": session["user_id"]})
+            # add cash
+            db.execute(text("UPDATE users SET cash = :c WHERE id = :id"),
+                       {"c": cash + shares * ppu, "id": session["user_id"]})
+            # redirect to main page when everything was done
+            return redirect("/")
 
 
 @ app.route("/pwdchange", methods=["GET", "POST"])
@@ -262,18 +271,19 @@ def pwdchange():
     if request.method == "GET":
         return render_template("pwdchange.html")
     else:
-        hash = db.execute("SELECT hash FROM users WHERE id = ?",
-                          session["user_id"])[0]['hash']
-        old_password = request.form.get("old_password")
-        new_password = request.form.get("new_password")
-        confirmation = request.form.get("confirmation")
-        if not old_password or not new_password or not confirmation:
-            return apology("Must provide old and new passwords", 403)
-        elif not check_password_hash(hash, old_password):
-            return apology("Invalid password", 403)
-        elif new_password != confirmation:
-            return apology("Passwords do not match!", 403)
-        new_hash = generate_password_hash(new_password)
-        db.execute(
-            "UPDATE users SET hash = ? WHERE id = ?", new_hash, session["user_id"])
-        return redirect("/")
+        with engine.begin() as db:
+            hash = db.execute(text("SELECT hash FROM users WHERE id = :id"),
+                              {"id": session["user_id"]}).all()[0]['hash']
+            old_password = request.form.get("old_password")
+            new_password = request.form.get("new_password")
+            confirmation = request.form.get("confirmation")
+            if not old_password or not new_password or not confirmation:
+                return apology("Must provide old and new passwords", 403)
+            elif not check_password_hash(hash, old_password):
+                return apology("Invalid password", 403)
+            elif new_password != confirmation:
+                return apology("Passwords do not match!", 403)
+            new_hash = generate_password_hash(new_password)
+            db.execute(text(
+                "UPDATE users SET hash = :h WHERE id = :id"), {"h": new_hash, "id": session["user_id"]})
+            return redirect("/")
