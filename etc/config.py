f9c7477067985e4bc9e92fdcece2c3ff18e3ab38
Hevsy <roomyn@gmail.com>
2023-01-14 04:50:14 +0000

    parameters are read from SSM - working

--- etc/config.py
+++ etc/config.py
@@ -1,24 +1,23 @@
 import boto3
 
 def param_get(param_name):
-# Create a client for the SSM service
-ssm = boto3.client('ssm')
+    # Create a client for the SSM service
+    ssm = boto3.client('ssm', region_name='us-east-1')
 
-# Get the value of the parameter
-return = ssm.get_parameter(
-    Name=param_name
-)
+    # Get the value of the parameter
+    return (ssm.get_parameter(Name=param_name, WithDecryption=True))['Parameter']['Value']
 
 
 db_config = {
     "db_type" : "postgresql+psycopg2", 
     "db_username" : "postgres", 
-    "db_pass" : "postgres-fin", 
+    "db_pass" : param_get('finance_db_pwd'), 
     "db_host" : "db-finance1.cxhmztea6vcv.us-east-1.rds.amazonaws.com", 
     "db_file" :"finance"
 }
 
-API_KEY = param_get('API_KEY')
+API_KEY = param_get('finance_api-key')
+# print (API_KEY, db_config)
 
 db_config_test = {
     "db_type" : "sqlite", 
