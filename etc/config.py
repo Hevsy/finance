7951a58c03453721d474d1833b6ef87c650a9d65
Hevsy <roomyn@gamil.com>
2023-02-02 07:47:13 +1100

    minor debaugging

--- etc/config.py
+++ etc/config.py
@@ -1,29 +1,30 @@
 import boto3
 
-def param_get(param_name):
-# Create a client for the SSM service
-ssm = boto3.client('ssm')
 
-# Get the value of the parameter
-return = ssm.get_parameter(
-    Name=param_name
-)
+def param_get(param_name):
+    # Create a client for the SSM service
+    ssm = boto3.client('ssm')
+
+    # Get the value of the parameter
+    return = ssm.get_parameter(
+        Name=param_name
+    )
 
 
 db_config = {
-    "db_type" : "postgresql+psycopg2", 
-    "db_username" : "postgres", 
-    "db_pass" : "postgres-fin", 
-    "db_host" : "db-finance1.cxhmztea6vcv.us-east-1.rds.amazonaws.com", 
-    "db_file" :"finance"
+    "db_type": "postgresql+psycopg2",
+    "db_username": "postgres",
+    "db_pass": "postgres-fin",
+    "db_host": "db-finance1.cxhmztea6vcv.us-east-1.rds.amazonaws.com",
+    "db_file": "finance"
 }
 
 API_KEY = param_get('API_KEY')
 
 db_config_test = {
-    "db_type" : "sqlite", 
-    "db_username" : None, 
-    "db_pass" : None, 
-    "db_host" : None, 
-    "db_file" :"finance.db"
+    "db_type": "sqlite",
+    "db_username": None,
+    "db_pass": None,
+    "db_host": None,
+    "db_file": "finance.db"
 }
